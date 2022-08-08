ee320b186cf8755de4c68b06cd6d74be73e5cd85
Oscar Vargas <oscar.vargas@ust.com>
2022-08-08 20:22:51 +0200

    Update User Method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Get, Post, Req, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Post, Put, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "@app/user/user.service";
 import { CreateUserDto } from "@app/user/dto/createUser.dto";
 import { UserResponseInterface } from "@app/user/types/userResponse.interface";
 import { LoginUserDto } from "@app/user/dto/loginUser.dto";
-import { ExpressRequest } from "@app/types/expressRequest.interface";
 import { User } from "@app/user/decorators/user.decorator";
-import { UserEntity } from "./user.entity";
+import { UserEntity } from "@app/user/user.entity";
+import { AuthGuard } from "@app/user/guards/auth.guard";
+import { UpdateUserDto } from "@app/user/dto/updateUser.dto";
 
 @Controller()
 export class UserController {
@@ -26,9 +27,20 @@ export class UserController {
   }
 
   @Get('user')
+  @UseGuards(AuthGuard)
   async currentUser(
     @User() user: UserEntity
   ): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Put('user')
+  @UseGuards(AuthGuard)
+  async updateUser(
+    @User('id') currentUserId: number,
+    @Body('user') updateUserDto: UpdateUserDto
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.updateUser(currentUserId, updateUserDto);
     return this.userService.buildUserResponse(user);
   }
 }
