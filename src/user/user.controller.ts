1c054b687a0ae129c1cc1b36920d1f46893b999c
Oscar Vargas <oscar.vargas@ust.com>
2022-08-08 13:14:50 +0200

    User module modified, added userDto and types. New migration added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "@app/user/user.service";
+import { CreateUserDto } from "@app/user/dto/createUser.dto";
+import { UserResponseInterface } from "@app/user/types/userResponse.interface";
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
   @Post('users')
-  async createUser(): Promise<any> {
-    return this.userService.createUser();
+  @UsePipes(new ValidationPipe())
+  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,52 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { CreateUserDto } from "@app/user/dto/createUser.dto";
+import { UserEntity } from "@app/user/user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { sign } from "jsonwebtoken";
+import { JWT_SECRET } from "@app/config";
+import { UserResponseInterface } from "@app/user/types/userResponse.interface";
 
 @Injectable()
 export class UserService {
-  async createUser() {
-    return 'create user from service'
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>
+  ) { }
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDto.email,
+    });
+    const userByUsername = await this.userRepository.findOne({
+      username: createUserDto.username,
+    });
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or username are taken',
+        HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
+    console.log('newUser', newUser);
+    return await this.userRepository.save(newUser);
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    }, JWT_SECRET,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user)
+      }
+    }
   }
 }
